98b0549874eb2062ba1235c66b1fd950d743dc4b
tomeichlersmith <eichl008@umn.edu>
2024-03-27 09:49:14 -0500

    add example histogram with a bunch of comments explaining bin choice

--- src/Ecal/TrigPrimResolutionAnalyzer.cxx
+++ src/Ecal/TrigPrimResolutionAnalyzer.cxx
@@ -24,12 +24,21 @@ class TrigPrimResolutionAnalyzer : public framework::Analyzer {
 };
 
 void TrigPrimResolutionAnalyzer::configure(framework::config::Parameters& parameters) {
-  digi_collection_name_ = parameters.getParameter("digi_collection_name", digi_collection_name_);
-  digi_pass_name_ = parameters.getParameter("digi_pass_name", digi_pass_name_);
-  trig_collection_name_ = parameters.getParameter("trig_collection_name", trig_collection_name_);
-  trig_pass_name_ = parameters.getParameter("trig_pass_name", trig_pass_name_);
-  hit_collection_name_ = parameters.getParameter("hit_collection_name", hit_collection_name_);
-  hit_pass_name_ = parameters.getParameter("hit_pass_name", hit_pass_name_);
+  /*
+   * Since I defined these member variables to have sensible values in the declaration above,
+   * we DO NOT get their parameter values from the python config. Below, I have written out
+   * the lines that we could use to get their values from the python config if changing them
+   * is desirable (e.g. if you want to run a few different passes of digi and recon with different
+   * parameters and then want to analyze the different passes).
+   */
+  /*
+  digi_collection_name_ = parameters.getParameter("digi_collection_name");
+  digi_pass_name_ = parameters.getParameter("digi_pass_name");
+  trig_collection_name_ = parameters.getParameter("trig_collection_name");
+  trig_pass_name_ = parameters.getParameter("trig_pass_name");
+  hit_collection_name_ = parameters.getParameter("hit_collection_name");
+  hit_pass_name_ = parameters.getParameter("hit_pass_name");
+  */
 }
 
 void TrigPrimResolutionAnalyzer::onProcessStart() {
@@ -37,7 +46,13 @@ void TrigPrimResolutionAnalyzer::onProcessStart() {
   // first, we get the directory for this processor in the histogram file
   getHistoDirectory();
   // then we can create histograms within it
+  histograms_.create(
+      "total_trig_energy" /* name - as written in output ROOT file */,
+      "Total of all Trig Digis" /* xlabel - axis label of histogram */,
+      1000 /* number of bins */, 0 /* minimum value */, 10000 /* maximum value */
+  ); // WARNING: I don't think this binning is good!! I just picked a random number!!
   /*
+   * 2D Histograms are also possible
   histograms_.create(
       "name",
       "xlabel", nbins, xstart, xend,
@@ -54,9 +69,33 @@ void TrigPrimResolutionAnalyzer::analyze(const framework::Event& event) {
   // digis are a ldmx::HgcrocDigiCollection
   const auto& hits  = event.getCollection<ldmx::EcalHit>(hit_collection_name_, hit_pass_name_);
   // hits are a std::vector<ldmx::EcalHit>
+
+  int total{0};
+  for (const auto& trig : trigs) {
+    total += trig.linearPrimitive();
+  }
+
+  /*
+   * after picking some dummy bins, I printout the values I'm going to fill
+   * so I can see what an actual binning should be. For 10 events, the output was
+   * 978
+   * 949
+   * 1070
+   * 858
+   * 846
+   * 774
+   * 853
+   * 859
+   * 887
+   * 917
+   * so I changed the binning to 1k bins ranging from 0 to 10k
+  std::cout << total << std::endl;
+   */
+  histograms_.fill("total_trig_energy", total);
   
   /*
-  histograms_.fill(xvalue, yvalue);
+   * 2D fill example
+  histograms_.fill("name", xvalue, yvalue);
   */
 }
 
